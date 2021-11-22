430e49454a26a902754a240b7f0b91ffa3a651e9
Morten Empeño <morten@empeno.dk>
2021-11-22 11:27:33 +0100

    intitial static web app

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  version = "=2.85.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "StaticAppWithTerraform"
+    storage_account_name = "dkterraformsa"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "StaticAppWithTerraform" {
+  name     = "StaticAppWithTerraform"
+  location = "westeurope"
+}
+
+resource "azurerm_static_site" "StaticAppWithTerraform" {
+  name                = "StaticAppWithTerraform"
+  resource_group_name = "StaticAppWithTerraform"
+  location            = "West Europe"
+}
