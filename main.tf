f1b22436202a26d026c653287ba5b55f2d76bb06
Morten Empeño <morten@empeno.dk>
2021-11-23 09:52:36 +0100

    tag with colon

--- main.tf
+++ main.tf
@@ -22,4 +22,7 @@ resource "azurerm_static_site" "StaticAppWithTerraform" {
   resource_group_name = azurerm_resource_group.StaticAppWithTerraformRG.name
   location            = azurerm_resource_group.StaticAppWithTerraformRG.location
   sku_tier            = "Free" 
+  tags = {
+        "owner": "Empeño"
+    }
 }
